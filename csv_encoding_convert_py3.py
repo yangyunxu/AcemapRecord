4faea661704b43201bf4b96325d1d57b85402926
Huang Yaowei <hywei1995@qq.com>
2017-12-17 20:36:31 +0800

    编码问题

--- csv_encoding_convert_py3.py
+++ csv_encoding_convert_py3.py
@@ -4,5 +4,7 @@
 import chardet
 stream = open('成员分组及每周工作进度.csv', 'rb').read()
 charset = chardet.detect(stream)['encoding']
+if charset == 'GB2312':
+	charset = 'GBK'
 open('成员分组及每周工作进度.csv', 'wb').write(stream.decode(charset).encode('utf_8_sig'))
 print('原编码为%s，已经调整为UTF8-BOM' % (charset if charset != 'UTF-8-SIG' else 'UTF8-BOM'))
